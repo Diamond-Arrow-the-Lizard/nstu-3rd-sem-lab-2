e2644e3409ba4ca703770d45bfce640216262e4e
Diamond_Arrow <diamond1arrow@gmail.com>
2024-10-23 09:16:32 +0700

    Added files to repo

---
+++ Program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Lab2
+{
+
+    public class Program {
+        public static void Main(string args[]) {
+
+            Console.WriteLine("Vasya");
+
+        }
+    }
+
+
+}
