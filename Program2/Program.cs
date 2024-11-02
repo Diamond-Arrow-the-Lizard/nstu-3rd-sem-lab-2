ba08bd5f38b5b266550e9963daa520c0791d74b4
Diamond_Arrow <diamond1arrow@gmail.com>
2024-11-02 11:02:23 +0700

    added interface prototype

--- Program2/Program.cs
+++ Program2/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+namespace lab2_part2 {
+    
+    interface IProduct
+    {
+        
+    }
+
+
+    
+    public class Program {
+
+        public static void Main(string[] args) {
+            
+        }
+    }
+
+}
