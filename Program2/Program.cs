7a9d58e23d8e53b86c672463bde9e3120a64cad7
Diamond_Arrow <diamond1arrow@gmail.com>
2024-11-04 17:10:22 +0700

    Printing Computer info works

--- Program2/Program.cs
+++ Program2/Program.cs
@@ -2,31 +2,60 @@
 
 namespace lab2_part2 {
     
-    interface IProduct
+    public abstract class IProduct
     {
         
-        virtual void PrintInfo() {
+        public virtual void PrintInfo() {
 
+            Console.WriteLine("Default info");
         }
         
     }
 
 
-    public abstract class Tech : IProduct {
+    public class Tech : IProduct {
+
+        protected string vendor {set; get;} = "None";
+        protected string name {set; get;} = "None";
+        protected int price {set; get;} = 0;
+
+        public override void PrintInfo() {
+            
+            Console.WriteLine("Vendor: {0}\nName: {1}\nPrice: {2}",
+                    vendor, name, price);
+        }
 
     }
 
     public sealed class Computer: Tech {
+        
+        string CPU {set; get;} = "None";
+        string GPU {set; get;} = "None";
+        string OS {set; get;} = "None";
+
+        public new void PrintInfo() {
+             
+            base.PrintInfo();
+            Console.WriteLine("CPU: {0}\nGPU: {1}\nOS: {2}",
+                    CPU, GPU, OS);
+        }
 
     }
 
     public sealed class Tablet: Tech {
+
+        public new void PrintInfo() {
+
+        }
 
     }
 
 
     public abstract class PrintingTool : Tech {
 
+        public new void PrintInfo() {
+
+        }
     }
 
     public sealed class Scanner : PrintingTool {
@@ -37,6 +66,10 @@ namespace lab2_part2 {
     public class Program {
 
         public static void Main(string[] args) {
+
+            Computer pc = new Computer();
+
+            pc.PrintInfo();
             
         }
 
--- Program2/obj/Debug/net8.0/Program2.AssemblyInfo.cs
+++ Program2/obj/Debug/net8.0/Program2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba08bd5f38b5b266550e9963daa520c0791d74b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4d53e6e67eeffaa9e3d0e35203ca34c1d0f3ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
