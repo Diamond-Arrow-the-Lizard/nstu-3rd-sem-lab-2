feeb05b27e41a00518184a131f9f20fb9716b41e
Diamond_Arrow <diamond1arrow@gmail.com>
2024-11-11 12:48:51 +0700

    ToString() overrides done

--- Program2/Program.cs
+++ Program2/Program.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 
 namespace lab2_part2 {
-    
-    public abstract class IProduct
-    {
-        protected string type {set; get;} = "Nan";
-        
-        
+
+    public abstract class Product {
+
+        protected string type {set; get;} = "None";
+
+        public override abstract string ToString();
+
     }
 
 
-    public class Tech : IProduct {
+    public class Tech : Product {
 
         protected string vendor {set; get;} = "None";
         protected string name {set; get;} = "None";
@@ -18,13 +19,20 @@ namespace lab2_part2 {
 
         public Tech() {
 
-            type = "Technology";
+            type = "Tech";
+        }
+
+        public override string ToString() {
+
+            string formattedOutput = $"Product type: {type}\nVendor: {vendor}\nProduct name: {name}\nPrice: {price}";
+            return formattedOutput;
+
         }
 
     }
 
     public class Computer: Tech {
-        
+
         protected string CPU {set; get;} = "None";
         protected string GPU {set; get;} = "None";
         protected string OS {set; get;} = "None";
@@ -32,6 +40,14 @@ namespace lab2_part2 {
         public Computer() {
 
             type = "Computer";
+        }
+
+        public override string ToString() {
+
+            string formattedOutput = $"Product type: {type}\nVendor: {vendor}\nProduct name: {name}\nPrice: {price}";
+            formattedOutput += $"\nCPU: {CPU}\nGPU: {GPU}\nOS: {OS}";
+            return formattedOutput;
+
         }
 
     }
@@ -45,16 +61,33 @@ namespace lab2_part2 {
             type = "Tablet";
         }
 
+        public override string ToString() {
+
+            string formattedOutput = $"Product type: {type}\nVendor: {vendor}\nProduct name: {name}\nPrice: {price}";
+            formattedOutput += $"\nCPU: {CPU}\nGPU: {GPU}\nOS: {OS}";
+            formattedOutput += $"\nHas Stylus support: {hasStylus}";
+            return formattedOutput;
+
+        }
+
     }
 
 
     public class PrintingTool : Tech {
-        
+
         protected bool isMulticolour {set; get;} = false;
 
         public PrintingTool() {
 
             type = "Printing Tool";
+        }
+
+        public override string ToString() {
+
+            string formattedOutput = $"Product type: {type}\nVendor: {vendor}\nProduct name: {name}\nPrice: {price}";
+            formattedOutput += $"\nCan print multicolour: {isMulticolour}";
+            return formattedOutput;
+
         }
 
     }
@@ -68,9 +101,18 @@ namespace lab2_part2 {
             type = "Scanner";
         }
 
+        public override string ToString() {
+
+            string formattedOutput = $"Product type: {type}\nVendor: {vendor}\nProduct name: {name}\nPrice: {price}";
+            formattedOutput += $"\nCan print multicolour: {isMulticolour}";
+            formattedOutput += $"\nScanner Type: {scannerType}";
+            return formattedOutput;
+
+        }
+
     }
 
-    
+
     public class Program {
 
         public static void Main(string[] args) {
@@ -79,7 +121,9 @@ namespace lab2_part2 {
             Tablet tab = new Tablet();
             Scanner scammer = new Scanner();
 
-            
+            Console.WriteLine($"{pc}\n\n{tab}\n\n{scammer}");
+
+
         }
 
     }
