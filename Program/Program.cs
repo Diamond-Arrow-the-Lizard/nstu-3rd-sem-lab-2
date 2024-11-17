2a4091df71b1933ba953572824d7816992632e7b
Diamond_Arrow <diamond1arrow@gmail.com>
2024-11-17 19:11:42 +0700

    Method removing negative numbers implemented

--- Program/Program.cs
+++ Program/Program.cs
@@ -67,6 +67,17 @@ namespace Lab2 {
 
     } // SymbolChecker extention end
 
+    public static class NegativeNumberDeletor {
+
+        public static Array deleteNegativeValues(this Array arr) {
+                int[] newArray = arr.array;
+                newArray = newArray.Where(val => val >= 0).ToArray();
+                arr.array = newArray;
+                return arr;
+            }
+
+    } // NegativeNumberDeletor extention end
+
     public class Program {
 
         public static void Main(string[] args) {
@@ -77,7 +88,7 @@ namespace Lab2 {
             a2.array = new int[]{1, 2, 3, 4, 5};
 
             Array a3 = new Array();
-            a3.array = new int[]{2, 3, 4, 5, 6};
+            a3.array = new int[]{-2, 3, 4, -5, 6};
 
             a1 = a2*a3; 
 
@@ -87,6 +98,11 @@ namespace Lab2 {
 
             Console.WriteLine(a1.hasSymbol('6'));
             Console.WriteLine(a1.hasSymbol('4'));
+
+            a1.deleteNegativeValues();
+            foreach (var item in a1.array) {
+                Console.WriteLine(item);
+            }
 
         }
 
