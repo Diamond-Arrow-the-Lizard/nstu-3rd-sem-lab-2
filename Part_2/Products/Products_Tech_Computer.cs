7c0f47840fb7226daf959f1ec0b9b26454136ba9
Diamond_Arrow <diamond1arrow@gmail.com>
2024-11-19 22:43:29 +0700

    Part 2 restructured

---
+++ Part_2/Products/Products_Tech_Computer.cs
@@ -0,0 +1,47 @@
+namespace Products;
+
+public class Computer: Tech {
+
+    public string CPU {set; get;} = "None";
+    public string GPU {set; get;} = "None";
+    public string OS {set; get;} = "None";
+
+    public Computer() {
+
+        Type = "Computer";
+    }
+
+    public override bool breakProduct() {
+        if(isBroken == true) { 
+            Console.WriteLine("It's already broken!");
+            return isBroken;
+        }
+
+        Console.WriteLine($"You broke {Name}. All the money for {CPU} and {GPU} for nothing..."); 
+        isBroken = true;
+        return isBroken;
+    }
+
+    public override bool repairProduct() {
+        if(isBroken == false) { 
+            Console.WriteLine("It's fine, no need for a repair");
+            return isBroken;
+        }
+
+        Console.WriteLine($"{Name} was repaired. It only cost you a kidney!");
+        isBroken = false;
+        return isBroken;
+    }
+
+    public override string ToString() {
+        if (isBroken == true) {
+            return "It's broken!\n";
+        }
+
+        string formattedOutput = $"Product Type: {Type}\nVendor: {Vendor}\nProduct Name: {Name}\nPrice: {Price}";
+        formattedOutput += $"\nCPU: {CPU}\nGPU: {GPU}\nOS: {OS}";
+        return formattedOutput + '\n';
+
+    }
+
+}
---
+++ Part_2/Products/Products_Tech_Computer_Tablet.cs
@@ -0,0 +1,53 @@
+namespace Products;
+
+public sealed class Tablet: Computer {
+
+    private bool _has_stylus = false;
+    bool hasStylus {
+        set {
+            _has_stylus = value;
+        }
+        get => _has_stylus;
+    } 
+
+    public Tablet() {
+
+        Type = "Tablet";
+    }
+
+    public override bool breakProduct() {
+        if(isBroken == true) { 
+            Console.WriteLine("It's already broken!");
+            return isBroken;
+        }
+
+        Console.WriteLine($"You broke {Name}. No more YouTube for you...");
+        isBroken = true;
+        return isBroken;
+    }
+
+    public override bool repairProduct() {
+        if(isBroken == false) { 
+            Console.WriteLine("It's fine, no need for a repair");
+            return isBroken;
+        }
+
+        Console.WriteLine($"{Name} was repaired and is ready to destroy your eyesight...");
+        isBroken = false;
+        return isBroken;
+    }
+
+    public override string ToString() {
+        if (isBroken == true) {
+            return "It's broken!\n";
+        }
+
+        string formattedOutput = $"Product Type: {Type}\nVendor: {Vendor}\nProduct Name: {Name}\nPrice: {Price}";
+        formattedOutput += $"\nCPU: {CPU}\nGPU: {GPU}\nOS: {OS}";
+        formattedOutput += $"\nHas Stylus support: {hasStylus}";
+        return formattedOutput + '\n';
+
+    }
+
+}
+
